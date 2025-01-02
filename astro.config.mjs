368d76d825f05a30b62fcf057f1490cc0c4bdc75
Yash Burshe <bursheyash2@gmail.com>
2025-01-01 22:41:48 -0500

    Update domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.yashburshe.com',
+  site: 'https://yashburshe.com',
   integrations: [tailwind()]
 });
