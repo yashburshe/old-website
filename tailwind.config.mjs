2332b01593cf66b33477cc97d33308ba66b5e6ad
Yash Burshe <bursheyash2@gmail.com>
2025-01-02 00:45:49 -0500

    Change tailwind screen sizes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,17 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    screens: {
+      '2xs': { min: '300px' },
+      xs: { max: '575px' },
+      sm: { min: '576px', max: '897px' },
+      md: { min: '898px', max: '1199px' },
+      lg: { min: '1200px' },
+      xl: { min: '1259px' },
+      '2xl': { min: '1359px' }
+    },
   },
   plugins: [
-	require('@tailwindcss/typography')
+    require('@tailwindcss/typography')
   ],
 };
