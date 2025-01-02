226cbfe517225d70a456aecfa3d0d58bc737fc7d
Yash Burshe <bursheyash2@gmail.com>
2025-01-02 01:05:46 -0500

    Adjust responsiveness

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,15 +2,6 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    screens: {
-      '2xs': { min: '300px' },
-      xs: { max: '575px' },
-      sm: { min: '576px', max: '897px' },
-      md: { min: '898px', max: '1199px' },
-      lg: { min: '1200px' },
-      xl: { min: '1259px' },
-      '2xl': { min: '1359px' }
-    },
   },
   plugins: [
     require('@tailwindcss/typography')
